56048298dd02f49e664219b2b2855edd81b854a3
VitaliiBalaiev <vitaliibalaiev@gmail.com>
2024-05-28 16:57:32 +0300

    Initial commit

---
+++ src/main/java/primenumbers/PrimeChecker.java
@@ -0,0 +1,17 @@
+package primenumbers;
+
+public class PrimeChecker {
+
+    public boolean isPrimeNumber(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/primenumbers/PrimeNumbers.java
@@ -0,0 +1,9 @@
+package primenumbers;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        PrimeChecker checker = new PrimeChecker();
+
+        System.out.println(checker.isPrimeNumber(5));
+    }
+}
