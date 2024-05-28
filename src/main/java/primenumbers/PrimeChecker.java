2a18d047d82223aa06868f5a2df405e1882d4e61
VitaliiBalaiev <vitaliibalaiev@gmail.com>
2024-05-28 17:45:38 +0300

    added validation to check if input is a number

--- src/main/java/primenumbers/PrimeChecker.java
+++ src/main/java/primenumbers/PrimeChecker.java
@@ -2,6 +2,19 @@ package primenumbers;
 
 public class PrimeChecker {
 
+    public boolean isValidNumber(String input) throws IllegalArgumentException {
+        int number;
+
+        try {
+            number = Integer.parseInt(input);
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Input is not a number");
+        }
+
+        return isPrimeNumber(number);
+    }
+
     public boolean isPrimeNumber(int number) {
         if (number <= 1) {
             return false;
